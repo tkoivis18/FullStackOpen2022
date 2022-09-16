d77db23a4005c36798151c94eeabfcc5130f2a1b
tkoivis18 <tobias.koivisto@gmail.com>
2022-09-17 01:21:56 +0300

    Tehtävät 1.3

--- Osa1/1.1step1/src/App.js
+++ Osa1/1.1step1/src/App.js
@@ -1,18 +1,20 @@
-  const course = 'Half Stack application development'
+/*   //const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
   const exercises2 = 7
   const part3 = 'State of a component'
-  const exercises3 = 14
+  const exercises3 = 14  */
 
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
+const Header = ({course}) => {
+  
+  return (    
+    <h1>{course}</h1>
   )
 }
 
-const Part = (props) => {
+/* const Part = (props) => {
+  
   return (
     <div>
     <p>
@@ -20,13 +22,23 @@ const Part = (props) => {
     </p>
     </div>
   )
-}
+} */
 
 
-const Content = (props) => {
+const Content = ({data1, data2, data3}) => {
   return (
     <div>
-      <Part 
+      <p>
+        {data1.name} {data1.exercises}
+      </p>
+      <p>
+        {data2.name} {data2.exercises}
+      </p>
+      <p>
+        {data3.name} {data3.exercises}
+      </p>
+      
+      {/* <Part 
         part={part1}
         exercises={exercises1}
       />
@@ -37,32 +49,48 @@ const Content = (props) => {
       <Part 
         part={part3}
         exercises={exercises3}
-      />
-      </div>
+      /> */}
+
+    </div>
   )
 }
 
-const Total = (props) => {
+const Total = ({exercises1, exercises2, exercises3}) => {
   return (
     <div>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {exercises1.exercises + exercises2.exercises + exercises3.exercises}</p>
     </div>
   )
 }
 
 const App = () => {
   
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
       <Content 
-        
+        data1={part1}
+        data2= {part2}
+        data3= {part3}
       />
       <Total 
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
+        exercises1={part1}
+        exercises2={part2}
+        exercises3={part3}
       />
     </div>
   )
